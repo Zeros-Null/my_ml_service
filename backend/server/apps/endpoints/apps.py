2d849d0076fba78bc3fd2431d0b6f65537243c3d
Zeros-Null <2879068550@qq.com>
2024-06-24 16:07:06 +0800

    endpoints models

---
+++ backend/server/apps/endpoints/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EndpointsConfig(AppConfig):
+    name = 'apps.endpoints'
+
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # add django rest framework
+    # apps
+    'apps.endpoints'
 ]
 
 MIDDLEWARE = [
